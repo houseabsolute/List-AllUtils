68329affda35a501b8fb6d16dfbba40bd105f883
Dave Rolsky <autarch@urth.org>
2009-01-23 15:04:23 -0600

    initial import

---
+++ lib/List/AllUtils.pm
@@ -0,0 +1,53 @@
+package List::AllUtils;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+List::AllUtils - The fantastic new List::AllUtils!
+
+=head1 SYNOPSIS
+
+XXX - change this!
+
+    use List::AllUtils;
+
+    my $foo = List::AllUtils->new();
+
+    ...
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+This class provides the following methods
+
+=head1 AUTHOR
+
+Dave Rolsky, C<< <autarch@urth.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-list-allutils@rt.cpan.org>,
+or through the web interface at L<http://rt.cpan.org>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
