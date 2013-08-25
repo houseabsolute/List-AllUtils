a06f35eefd7a76c7b33357caefc29d079310b2d5
Dave Rolsky <autarch@urth.org>
2013-08-25 12:32:06 -0500

    Require List::Util 1.31 and copy docs from that release

--- lib/List/AllUtils.pm
+++ lib/List/AllUtils.pm
@@ -7,6 +7,7 @@ use warnings;
 BEGIN
 {
     require List::Util;
+    List::Util->VERSION(1.1);
     List::Util->import( @List::Util::EXPORT_OK );
 }
 use List::MoreUtils 0.28 qw( :all );
@@ -43,9 +44,38 @@ defined in C<List::Util> or C<List::MoreUtils>? I sure am. Now you
 don't have to remember. This module will export all of the functions
 that either of those two modules defines.
 
-=head1 FUNCTIONS
+Note that all function documentation has been shamelessly copied from
+L<List::Util> and <List::MoreUtils>.
 
-(Shamelessly copied from List::Util and List::MoreUtils ...)
+=head1 LIST-REDUCTION FUNCTIONS
+
+The following set of functions all reduce a list down to a single value.
+
+=head2 reduce BLOCK LIST
+
+Reduces LIST by calling BLOCK, in a scalar context, multiple times,
+setting C<$a> and C<$b> each time. The first call will be with C<$a>
+and C<$b> set to the first two elements of the list, subsequent
+calls will be done by setting C<$a> to the result of the previous
+call and C<$b> to the next element in the list.
+
+Returns the result of the last call to BLOCK. If LIST is empty then
+C<undef> is returned. If LIST only contains one element then that
+element is returned and BLOCK is not executed.
+
+    $foo = reduce { $a < $b ? $a : $b } 1..10       # min
+    $foo = reduce { $a lt $b ? $a : $b } 'aa'..'zz' # minstr
+    $foo = reduce { $a + $b } 1 .. 10               # sum
+    $foo = reduce { $a . $b } @bar                  # concat
+
+If your algorithm requires that C<reduce> produce an identity value, then
+make sure that you always pass that identity value as the first argument to prevent
+C<undef> being returned
+
+  $foo = reduce { $a + $b } 0, @values;             # sum with 0 identity value
+
+The remaining list-reduction functions are all specialisations of this
+generic idea.
 
 =head2 first BLOCK LIST
 
@@ -60,7 +90,7 @@ C<undef> is returned.
 
 This function could be implemented using C<reduce> like this
 
-    $foo = reduce { wanted($a) ? $a : wanted($b) ? $b : undef } undef, @list
+    $foo = reduce { defined($a) ? $a : wanted($b) ? $b : undef } undef, @list
 
 for example wanted() could be defined() which would return the first
 defined value in @list
@@ -119,29 +149,6 @@ This function could be implemented using C<reduce> like this
 
     $foo = reduce { $a lt $b ? $a : $b } 'A'..'Z'
 
-=head2 reduce BLOCK LIST
-
-Reduces LIST by calling BLOCK, in a scalar context, multiple times,
-setting C<$a> and C<$b> each time. The first call will be with C<$a>
-and C<$b> set to the first two elements of the list, subsequent
-calls will be done by setting C<$a> to the result of the previous
-call and C<$b> to the next element in the list.
-
-Returns the result of the last call to BLOCK. If LIST is empty then
-C<undef> is returned. If LIST only contains one element then that
-element is returned and BLOCK is not executed.
-
-    $foo = reduce { $a < $b ? $a : $b } 1..10       # min
-    $foo = reduce { $a lt $b ? $a : $b } 'aa'..'zz' # minstr
-    $foo = reduce { $a + $b } 1 .. 10               # sum
-    $foo = reduce { $a . $b } @bar                  # concat
-
-=head2 shuffle LIST
-
-Returns the elements of LIST in a random order
-
-    @cards = shuffle 0..51      # 0..51 in a random order
-
 =head2 sum LIST
 
 Returns the sum of all the elements in LIST. If LIST is empty then
@@ -154,6 +161,118 @@ C<undef> is returned.
 This function could be implemented using C<reduce> like this
 
     $foo = reduce { $a + $b } 1..10
+
+=head2 sum0 LIST
+
+Similar to C<sum>, except this returns 0 when given an empty list, rather
+than C<undef>.
+
+=cut
+
+=head1 KEY/VALUE PAIR LIST FUNCTIONS
+
+The following set of functions, all inspired by L<List::Pairwise>, consume
+an even-sized list of pairs. The pairs may be key/value associations from a
+hash, or just a list of values. The functions will all preserve the original
+ordering of the pairs, and will not be confused by multiple pairs having the
+same "key" value - nor even do they require that the first of each pair be a
+plain string.
+
+=cut
+
+=head2 pairgrep BLOCK KVLIST
+
+Similar to perl's C<grep> keyword, but interprets the given list as an
+even-sized list of pairs. It invokes the BLOCK multiple times, in scalar
+context, with C<$a> and C<$b> set to successive pairs of values from the
+KVLIST.
+
+Returns an even-sized list of those pairs for which the BLOCK returned true
+in list context, or the count of the B<number of pairs> in scalar context.
+(Note, therefore, in scalar context that it returns a number half the size
+of the count of items it would have returned in list context).
+
+    @subset = pairgrep { $a =~ m/^[[:upper:]]+$/ } @kvlist
+
+Similar to C<grep>, C<pairgrep> aliases C<$a> and C<$b> to elements of the
+given list. Any modifications of it by the code block will be visible to
+the caller.
+
+=head2 pairfirst BLOCK KVLIST
+
+Similar to the C<first> function, but interprets the given list as an
+even-sized list of pairs. It invokes the BLOCK multiple times, in scalar
+context, with C<$a> and C<$b> set to successive pairs of values from the
+KVLIST.
+
+Returns the first pair of values from the list for which the BLOCK returned
+true in list context, or an empty list of no such pair was found. In scalar
+context it returns a simple boolean value, rather than either the key or the
+value found.
+
+    ( $key, $value ) = pairfirst { $a =~ m/^[[:upper:]]+$/ } @kvlist
+
+Similar to C<grep>, C<pairfirst> aliases C<$a> and C<$b> to elements of the
+given list. Any modifications of it by the code block will be visible to
+the caller.
+
+=head2 pairmap BLOCK KVLIST
+
+Similar to perl's C<map> keyword, but interprets the given list as an
+even-sized list of pairs. It invokes the BLOCK multiple times, in list
+context, with C<$a> and C<$b> set to successive pairs of values from the
+KVLIST.
+
+Returns the concatenation of all the values returned by the BLOCK in list
+context, or the count of the number of items that would have been returned
+in scalar context.
+
+    @result = pairmap { "The key $a has value $b" } @kvlist
+
+Similar to C<map>, C<pairmap> aliases C<$a> and C<$b> to elements of the
+given list. Any modifications of it by the code block will be visible to
+the caller.
+
+=head2 pairs KVLIST
+
+A convenient shortcut to operating on even-sized lists of pairs, this
+function returns a list of ARRAY references, each containing two items from
+the given list. It is a more efficient version of
+
+    pairmap { [ $a, $b ] } KVLIST
+
+It is most convenient to use in a C<foreach> loop, for example:
+
+    foreach ( pairs @KVLIST ) {
+       my ( $key, $value ) = @$_;
+       ...
+    }
+
+=head2 pairkeys KVLIST
+
+A convenient shortcut to operating on even-sized lists of pairs, this
+function returns a list of the the first values of each of the pairs in
+the given list. It is a more efficient version of
+
+    pairmap { $a } KVLIST
+
+=head2 pairvalues KVLIST
+
+A convenient shortcut to operating on even-sized lists of pairs, this
+function returns a list of the the second values of each of the pairs in
+the given list. It is a more efficient version of
+
+    pairmap { $b } KVLIST
+
+=head1 OTHER FUNCTIONS
+
+=head2 shuffle LIST
+
+Returns the elements of LIST in a random order
+
+    @cards = shuffle 0..51      # 0..51 in a random order
+
+=head1 List::MoreUtils FUNCTIONS
 
 =head2 any BLOCK LIST
 
@@ -222,7 +341,7 @@ is true. Sets C<$_> for each item in LIST in turn:
     printf "item with index %i in list is 4", firstidx { $_ == 4 } @list;
     __END__
     item with index 1 in list is 4
-    
+
 Returns C<-1> if no such item could be found.
 
 C<first_index> is an alias for C<firstidx>.
